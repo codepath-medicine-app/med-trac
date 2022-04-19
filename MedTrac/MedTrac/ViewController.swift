f9c54b253c9609fb2ec243f4bc089420cc9b79e1
Melisa Calderon <mecalderon@vassar.edu>
2022-04-19 18:46:19 -0400

    starting code

---
+++ MedTrac/MedTrac/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MedTrac
+//
+//  Created by Melisa Calderon on 4/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
